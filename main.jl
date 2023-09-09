3639377be40aeb0f56271dffc40bbc629fc6805e
ZhizhchenkoA <sashazh@yahoo.com>
2023-09-09 13:20:03 +0300

    created julia notebook

---
+++ main.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.27
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f654666c-36bb-4854-ae63-3f0a1bb0343f
+using Plots
+
+# ╔═╡ Cell order:
+# ╠═f654666c-36bb-4854-ae63-3f0a1bb0343f
